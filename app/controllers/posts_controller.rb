fb09cf7d257c9f4697db003ab9d9f6b242b4d591
shengyang <shengyang@birchbox.com>
2020-02-06 11:55:35 -0500

    intial app commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+	def index
+		
+	end
+
+	def new 
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+			render "new"
+		end
+		
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end	
+end
